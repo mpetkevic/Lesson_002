f2a4f42a23123005d3c7fd38f991c7d2edb609b5
Miroslav P <miroslav.petkevic@gmail.com>
2018-02-25 16:08:21 +0200

    All homework + README.md

--- index.php
+++ index.php
@@ -94,6 +94,7 @@ echo "Задание 6" . "<br>";
 $bmw = ["name" => "X5", "speed" => 120, "doors" => 4, "year" => 2015];
 $toyota = ["name" => "Corolla", "speed" => 100, "doors" => 4, "year" => 2017];
 $opel = ["name" => "Astra", "speed" => 130, "doors" => 4, "year" => 2012];
+$cars = [];
 $cars["BMW"] = $bmw;
 $cars["TOYOTA"] = $toyota;
 $cars["OPEL"] = $opel;
